7ab79be8219d01d4276e2b23b6aa7448c7ea05f9
ZHANG Banruo <id1955@DOMENSAI.ECOLE>
2022-10-26 17:05:38 +0200

    gerer un table v0.1

--- src/view/gerer_table_vue.py
+++ src/view/gerer_table_vue.py
@@ -12,31 +12,53 @@ from view.session import Session
 from business_object.joueur import Joueur
 
 
-class s(VueAbstraite):
+class GererTableVue(VueAbstraite):
+
     def __init__(self, message=""):
         joueur = Session().user
-        # demander la séance a rejoindre
-        self.questions = [
-            {
-                "type": "input",
-                "name": "seance",
-                "message": "A quelles séance assistez-vous ?"}
-        ]
-        # demander le scenario et info complement
 
+        self.questions = [
+            {   # demander le séance a rejoindre
+                "type": "list",
+                "name": "seance",
+                "message": "Sélectionner la séance à laquelle vous souhaitez participer",
+                "choices": ["1. samedi matin",
+                            "2. samedi après-midi",
+                            "3. dimanche matin",
+                            "4. dimanche après-midi"]},
+            {   # demander le scenario
+                "type": "input",
+                "name": "scenario",
+                "message": "Indiquez le scénario que vous voulez diriger :"},
+            {   # demander l'information complementaire
+                "type": "input",
+                "name": "info_comple",
+                "message": "Si vous souhaitez ajouter quelque chose, écrivez ici."
+            }
+        ]
         self.message = message
 
     def afficher(self):
-        self.nettoyer_console()
+        self.nettoyer_console()  # laisser suffisamment d'espace avec la vue precedente
         print(self.message)
-        print()
+        print()  # nous laisse un espace
 
     def choisir_menu(self):
-        joueur = Session().user
-        # Si la suppression n a pas ete confirmee, on retourne au menu joueur
-        answers = prompt(self.questions)
-        confirm = answers["confirmation"]
-        message = ""
+        mj = Session().user
+        # recuperer les reponses saisies par le maitre de jeu
+        reponse = prompt(self.questions)
+        seance = int(reponse["seance"][0])
+        scenario = reponse["scenario"]
+        info_comple = reponse["info_comple"]
+
+        # verifier que si MJ est libre pour la sceance
+        if MaitreJeuService().dispo_mj == False:
+            message = "Vous ne pouvez pas jouer à tables en même temps, veuillez vérifier pour quelle table vous vous êtes inscrit et la séance correspondant"
+            from view.maitre_jeu_menu_vue import MaitreJeuMenuVue
+            return MaitreJeuMenuVue(message)
+
+
+# en-dessus a verifier, ceux pour maitrejeusevice.gerertable()...... a faire
 
         # La suppression est annulée en l'absence de confirmation
         if not confirm:
