b4e8895cdb620ac2d2b77ae27d97f4f7bd33d2c3
Banruo Zhang <banruo.zhang@eleve.ensai.fr>
2022-11-22 16:10:57 +0100

    ...

--- src/tests/test_service/test_table_jeu_service.py
+++ src/tests/test_service/test_table_jeu_service.py
@@ -41,49 +41,58 @@ class TestTableJeuService(unittest.TestCase):
 
     def test_creer(self):
         # GIVEN
-        id_seance = 1000
-        id_table = 9999
-        table_jeu = TableJeu(id_table, id_seance)
+        # creer une table en avance avec les personnages pour le test ?
+        id_seance = 1
+        id_table = 1000
 
         # WHEN
         nouvelle_table = TableJeuService().creer(id_seance)
 
         # THEN
+        # vérification de l'existance de la table qui vient d'être crée
         self.assertIsNotNone(nouvelle_table)
+        # vérification de l'égalité des valeurs de id_seance
         self.assertEqual(nouvelle_table.id_seance, id_seance)
 
     def test_supprimer(self):
         # GIVEN
         id_seance = 1
-        id_table = 1003
+        id_table = 1000
         table_jeu = TableJeu(id_table, id_seance)
 
         # WHEN
         statut = TableJeuService().supprimer(table_jeu)
 
         # THEN
+        # vérification de la suppression de la table_jeu
         self.assertTrue(statut)
 
     def test_trouver_par_id(self):
         # GIVEN
-        t1 = TableJeu(id_table=1000, id_seance=1000)
+        t1 = TableJeu(id_table=1000, id_seance=1)
 
         # WHEN
         t2 = TableJeuService().trouver_par_id(t1.id_table)
 
         # THEN
+        # vérification de ces 2 tables référence un même objet table
         self.assertEqual(t1.id_table, t2.id_table)
 
     def test_lister_personnages(self):
+
         # GIVEN
-        table_jeu = TableJeu(id_table=1000, id_seance=1000)
+        table_jeu = TableJeu(id_table=1000, id_seance=1)
 
         # WHEN
         liste_personnages = TableJeuService().lister_personnages(table_jeu)
 
         # THEN
+        # a modifier ? si l'on peut verifier le contenu de la liste de personnage ?
         self.assertEqual(len(liste_personnages), 2)
 
+    """def test_lister_tables_vides (self) : """
+
+    def test_lister(self):
     '''Suppression en BDD des données test'''
     @classmethod
     def tearDownClass(self):
