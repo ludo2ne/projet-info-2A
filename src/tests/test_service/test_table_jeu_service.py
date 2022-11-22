e9eaef84aa111aad23b8cafe20d35e85724147c3
Banruo Zhang <banruo.zhang@eleve.ensai.fr>
2022-11-22 17:35:45 +0100

    test_table_jeu_service mja, à tester

--- src/tests/test_service/test_table_jeu_service.py
+++ src/tests/test_service/test_table_jeu_service.py
@@ -19,6 +19,9 @@ from dao.db_connection import DBConnection
 
 from business_object.table_jeu import TableJeu
 from business_object.personnage import Personnage
+from business_object.maitre_jeu import MaitreJeu
+
+from business_object.joueur import Joueur
 
 
 @mock.patch.dict(os.environ, {"NB_JOUEURS_MAX_PAR_TABLE": "5"})
@@ -90,9 +93,67 @@ class TestTableJeuService(unittest.TestCase):
         # a modifier ? si l'on peut verifier le contenu de la liste de personnage ?
         self.assertEqual(len(liste_personnages), 2)
 
-    """def test_lister_tables_vides (self) : """
+    def test_lister_tables_vides(self):
+        # GIVEN
+        nb_table_vide = 5
+
+        # WHEN
+        liste_table_vide = TableJeuService().lister_tables_vides()
+
+        # THEN
+        self.assertEqual(len(liste_table_vide), nb_table_vide)
 
     def test_lister(self):
+        # GIVEN
+        pseudo = "toto"
+        # on utilise un joueur est inscrit dans notre base de données pour faire le test
+        joueur = JoueurService().trouver_par_pseudo(pseudo)
+        seance = 1
+        complete = True
+
+        # WHEN
+        liste_table1 = TableJeuService().lister(joueur)
+        liste_table2 = TableJeuService().lister(seance)
+        liste_table3 = TableJeuServie().lister(joueur, seance, complete)
+        liste_table4 = TableJeuService().lister(joueur, seance)
+
+        # THEN
+        # valeur pour la comparaison a verifier
+        self.assertEqual(len(liste_table1), 2)
+        self.assertEqual(len(liste_table2), 3)
+        self.assertEqual(len(liste_table3), 5)  # idée : erreur ici
+        self.assertEqual(len(liste_table4), 2)
+
+    def test_est_disponible(self):
+        # GIVEN
+        table_jeu1 = TableJeu(id_table=2, id_seance=1)
+        table_jeu2 = TableJeu(id_table=1000, id_seance=1)
+
+        # WHEN
+        dispo1 = TableJeuService().est_disponible(table_jeu1)
+        dispo2 = TableJeuService().est_disponible(table_jeu2)
+
+        # THEN
+        self.assertEqual(dispo1, True)  # valeur pour la comparaison a verifier
+        self.assertEqual(dispo2, False)  # idée : erreur ici
+
+    """def test_affichage_liste(self):          pour l'instant, aucune idée"""
+
+    def test_trouver_mj(self):
+        # GIVEN
+        mj1 = MaitreJeu("Ahj", "DUPONT", "Alice", "alice.dupont@gmail.com")
+        mj2 = MaitreJeu("Bha", "BATON", "Pierre", "pierre.baton@gmail.com")
+        table_jeu1 = TableJeu(id_table=2, id_seance=1, maitre_jeu=mj1)
+        table_jeu2 = TableJeu(id_table=1000, id_seance=1, maitre_jeu=mj2)
+
+        # WHEN
+        mj1r = TableJeuService().est_disponible(table_jeu1)
+        mj2r = TableJeuService().est_disponible(table_jeu2)
+
+        # THEN
+        self.assertEqual(mj1r, mj1)  # valeur pour la comparaison a verifier
+        self.assertEqual(mj2r, mj2)  # idée : erreur ici
+
     '''Suppression en BDD des données test'''
     @classmethod
     def tearDownClass(self):
